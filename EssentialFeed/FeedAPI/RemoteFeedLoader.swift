8045cd514d05707702fe4b0b8465ca5254e327dc
Ayumi <ayumi.highroof@gmail.com>
2021-12-23 15:50:13 +0900

    Replace magic number with variable given extra context

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -52,14 +52,6 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemsMapper {
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.item }
-    }
-    
     private struct Root: Decodable {
         let items: [Item]
     }
@@ -77,5 +69,14 @@ private class FeedItemsMapper {
                             imageURL: image)
         }
     }
-
+    
+    static var OK_200: Int { return 200 }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
+    }
 }
