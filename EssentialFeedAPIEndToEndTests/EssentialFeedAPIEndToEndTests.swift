dd8525f2d18c2b2549223ec9fbcd65caf369cc2d
Ayumi <ayumi.highroof@gmail.com>
2022-03-12 12:02:32 +0900

    Replace 'LoadFeedResult' with 'Swift.Result<[FeedImage], Error>' because 'LoadFeedResult' is deprecated

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -30,13 +30,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     // MARK: - Helpers
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> Swift.Result<[FeedImage], Error>? {
         let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerUrl, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
-        var receivedResult: LoadFeedResult?
+        var receivedResult: Swift.Result<[FeedImage], Error>?
         let exp = XCTestExpectation(description: "Wait for load completion")
         
         loader.load { result in
