bcd80f26ab230b8a76d5f058ce94f4643f1ee733
Ayumi <ayumi.highroof@gmail.com>
2022-03-23 10:47:15 +0900

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by 宇高あゆみ on 2022/03/16.
+//
+
+import UIKit
+
+final public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()    
+}
