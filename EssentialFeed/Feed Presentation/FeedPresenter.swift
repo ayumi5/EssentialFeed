9ace1927727c865b9931d13b9ab068d6ecb41a0a
Ayumi <ayumi.highroof@gmail.com>
2022-04-13 16:50:53 +0900

    Move `FeedErrorViewModel` to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -4,20 +4,6 @@
 //
 //  Created by 宇高あゆみ on 2022/04/13.
 //
-
-
-public struct FeedErrorViewModel {
-    public let message: String?
-    
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
