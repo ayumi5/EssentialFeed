44fba3d3dd1942488a19961b160681b151302d45
Ayumi <ayumi.highroof@gmail.com>
2022-04-14 15:45:57 +0900

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by 宇高あゆみ on 2022/04/14.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    var hasLocation: Bool {
+        return location != nil
+    }
+}
