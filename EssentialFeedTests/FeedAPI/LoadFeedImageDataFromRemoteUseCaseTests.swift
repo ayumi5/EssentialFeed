429307a74332dd0300088ee855dbce8b83b8f4d6
Ayumi <ayumi.highroof@gmail.com>
2022-04-21 11:54:42 +0900

    Remove `@discardableResult` attribute  - `FeedImageDataLoaderTask` instances should be properly managed and never discarded.

--- EssentialFeedTests/FeedAPI/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/FeedAPI/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -20,7 +20,7 @@ class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_loadImageDataFromURL_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        sut.loadImageData(from: url) { _ in }
+        _ = sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -28,8 +28,8 @@ class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        sut.loadImageData(from: url) { _ in }
-        sut.loadImageData(from: url) { _ in }
+        _ = sut.loadImageData(from: url) { _ in }
+        _ = sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
@@ -76,7 +76,7 @@ class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
         var sut: RemoteFeedImageDataLoader? = RemoteFeedImageDataLoader(client: client)
         var capturedResults = [FeedImageDataLoader.Result]()
         
-        sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
+        _ = sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
         sut = nil
         client.complete(withStatusCode: 200, data: anyData())
         
@@ -124,7 +124,7 @@ class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     private func expect(_ sut: RemoteFeedImageDataLoader, toReceiveResult expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let url = URL(string: "https://a-given-url.com")!
         let exp = XCTestExpectation(description: "Wait for load completion")
-        sut.loadImageData(from: url) { receivedResult in
+        _ = sut.loadImageData(from: url) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
