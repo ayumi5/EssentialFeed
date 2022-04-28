e1a053a9cefe02529588d620e7b3ec4a1c2e369b
Ayumi <ayumi.highroof@gmail.com>
2022-04-28 12:06:54 +0900

    Extract uniqueFeed factory helper into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -36,9 +36,6 @@ class FeedLoaderCacheDecoratorTests: XCTestCase {
     }
         
     // MARK: Helpers
-    private func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "https://any-url.com")!)]
-    }
     
     private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
