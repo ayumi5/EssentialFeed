a991963aebaae172940f36b45522d6c5a2ff44e5
Ayumi <ayumi.highroof@gmail.com>
2022-05-16 15:34:23 +0900

    Use iPhone 13 snapshot configuration for snapshot tests to fix an failure on CI server due to mismatching device versions

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -30,6 +30,24 @@ struct SnapshotConfiguration {
                 .init(userInterfaceStyle: style)
             ]))
     }
+    
+    static func iPhone13(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+        return SnapshotConfiguration(
+            size: CGSize(width: 390, height: 844),
+            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
+            layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
+            traitCollection: UITraitCollection(traitsFrom: [
+                .init(forceTouchCapability: .available),
+                .init(layoutDirection: .leftToRight),
+                .init(preferredContentSizeCategory: .medium),
+                .init(userInterfaceIdiom: .phone),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(displayScale: 3),
+                .init(displayGamut: .P3),
+                .init(userInterfaceStyle: style)
+            ]))
+    }
 }
 
 final class SnapshotWindow: UIWindow {
