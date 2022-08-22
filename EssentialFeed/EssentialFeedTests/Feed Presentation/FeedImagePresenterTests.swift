242a000974ff702bd77ebd7082b3ed99450a486f
Ayumi <ayumi.highroof@gmail.com>
2022-08-22 16:13:26 +0900

    Add FeedImagePresenter map

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -9,6 +9,13 @@ import XCTest
 import EssentialFeed
 
 class FeedImagePresenterTests: XCTestCase {
+    func test_map_createsViewModel() {
+        let feedImage = uniqueImage()
+        let viewModel = FeedImagePresenter<ViewSpy, AnyImage>.map(feedImage)
+        XCTAssertEqual(viewModel.description, feedImage.description)
+        XCTAssertEqual(viewModel.location, feedImage.location)
+    }
+    
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
         
