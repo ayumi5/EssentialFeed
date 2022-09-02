65933e318f2d1f695203344bcfb6379c0ed66625
Ayumi <ayumi.highroof@gmail.com>
2022-09-02 10:14:02 +0900

    Remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -23,8 +23,12 @@ internal class ManagedCache: NSManagedObject {
         return try context.fetch(request).first
     }
     
+    static func deleteCache(in context: NSManagedObjectContext) throws {
+        try find(in: context).map(context.delete).map(context.save)
+    }
+    
     internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-        try find(in: context).map(context.delete)
+        try deleteCache(in: context)
         return ManagedCache(context: context)
     }
 }
